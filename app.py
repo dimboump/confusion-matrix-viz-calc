fdb4b367e3c073d3a8390d59a2bb2055533347a2
dimboump <dimboump@pm.me>
2022-11-07 02:06:14 +0100

    Update URL, title, and metrics display

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 
+from typing import List
+from typing import Optional
+
 import streamlit as st
 import numpy as np
-import pandas as pd
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
+from matplotlib.pyplot import rc_context
+from matplotlib import pyplot as plt
 from sklearn.metrics import ConfusionMatrixDisplay
 
-st.title("Confusion Matrix Visualizer")
+st.title("Confusion Matrix Generator")
 
 st.sidebar.subheader("Enter the desired values:")
 tp = st.sidebar.number_input("True Positives" , value=0, min_value=0)
@@ -25,17 +27,30 @@ with st.sidebar.expander("Show advanced options"):
 
 # Main section
 cm = np.array([[tp, fp], [fn, tn]])
-cm_disp = (
-        ConfusionMatrixDisplay(cm, display_labels=display_labels)
-        .plot(cmap=cmap)
-    )
-cm_disp.ax_.set_title(cm_title)
-st.write(cm_disp.figure_)
+
+def plot_cm(
+    cm: np.ndarray, 
+    *,
+    title: Optional[str] = None, 
+    display_labels: List[str] = None, 
+    cmap: str = "Blues"
+) -> None:
+    if display_labels is None:
+        display_labels = ["0", "1"]
+
+    fig, ax = plt.subplots()
+    disp = ConfusionMatrixDisplay(cm, display_labels=display_labels)
+    disp.plot(cmap=cmap, ax=ax)
+    ax.set_title(title)
+    st.pyplot(fig)
+
+with st.container():
+    plot_cm(cm, title=cm_title, display_labels=display_labels, cmap=cmap)
 
 st.subheader("Metrics")
 
 @st.cache
-def calculate_metrics(tp: int, fp: int, fn: int, tn: int) -> tuple:
+def calculate_metrics(tp: int, fp: int, fn: int, tn: int) -> dict:
     try:
         accuracy = (tp + tn) / (tp + fp + fn + tn)
         precision = tp / (tp + fp)
@@ -43,14 +58,15 @@ def calculate_metrics(tp: int, fp: int, fn: int, tn: int) -> tuple:
         f1 = 2 * (precision * recall) / (precision + recall)
     except ZeroDivisionError:
         accuracy = precision = recall = f1 = 0
-    return (
-        round(accuracy, 3),
-        round(precision, 3),
-        round(recall, 3),
-        round(f1, 3)
-    )
+    return {
+        "Accuracy: ": accuracy,
+        "Precision: ": precision,
+        "Recall: ": recall,
+        "F1: ": f1
+    }
 
-st.write("Accuracy: ", calculate_metrics(tp, fp, fn, tn)[0])
-st.write("Precision: ", calculate_metrics(tp, fp, fn, tn)[1])
-st.write("Recall: ", calculate_metrics(tp, fp, fn, tn)[2])
-st.write("F1: ", calculate_metrics(tp, fp, fn, tn)[3])
+metrics = calculate_metrics(tp, fp, fn, tn)
+col1, col2, col3, col4 = st.columns(4)
+for col, metric, score in zip((col1, col2, col3, col4), metrics.keys(), metrics.values()):
+    with col:
+        st.metric(label=metric, value=round(score, 3))
